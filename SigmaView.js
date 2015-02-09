8ce7653a2420a1d75e5dba67ff037d3f39274a04
Jaron <jaronpiet@t-online.de>
2015-02-09 13:26:53 +0100

    Optimized layout
    SigmaView now produces a tree layout depending on the node references.

--- SigmaView.js
+++ SigmaView.js
@@ -7,8 +7,10 @@ function NewNode(nodeList)
         edges: []
     };
    
+    var LabelOffsetFactor = 5.5;
+
     nodeList.sort(SortByRank);
-    var maxReferences = nodeList[0].NumReferences;
+    var maxReferences = nodeList[0].NumReferences+1;
     var graphLevelMatrix = new Array(maxReferences);
 
     for (i = 0; i < nodeList.length; i++)
@@ -25,18 +27,19 @@ function NewNode(nodeList)
         var numLevelNodes = 0;
         if (graphLevelMatrix[level] != null)
             numLevelNodes = graphLevelMatrix[level].length;
-        else
-            numLevelNodes = 0;
 
-        var xStart = -numLevelNodes / 2;
+        var widthVC = document.getElementById("ViewContainer").clientWidth;
+        var nodeStep = widthVC / (numLevelNodes);
+
+        var xPos = -(nodeStep * (numLevelNodes+1)/ 2);
+        var yPos = 0;
         for (i = 0; i < numLevelNodes; i++)
         {
             var node = graphLevelMatrix[level][i];
             var nodeName = node.Name;
 
-
-            var xPos = xStart+i*nodeName.length * 10;
-            var yPos = level - 10;
+            xPos += nodeStep;
+            yPos = - level * 100;
 
             g.nodes.push({
                 id: nodeName,
@@ -44,9 +47,9 @@ function NewNode(nodeList)
                 x: xPos,
                 y: yPos,
                 size: level + 5,
-                'color': 'rgb(' + Math.round(0) + ',' +
-                                  Math.round(yPos * 5) + ',' +
-                                  Math.round(256) + ')'
+                'color': 'rgb(' + Math.round(level * 30) + ',' +
+                                  Math.round(256) + ',' +
+                                  Math.round(level * 30) + ')'
             });
 
             for (j = 0; j < node.NumReferences; j++)
@@ -54,9 +57,9 @@ function NewNode(nodeList)
                     id: 'e' + nodeName + j,
                     source: node.References[j],
                     target: nodeName,
-                    size: Math.random(),
-                    'color': 'rgb(' + Math.round(0) + ',' +
-                                      Math.round(yPos * 5) + ',' +
+                    size: 0.1,
+                    'color': 'rgb(' + Math.round(level * 20) + ',' +
+                                      Math.round(level * 50) + ',' +
                                       Math.round(256) + ')'
                 });
         }
@@ -68,7 +71,7 @@ function NewNode(nodeList)
     s = new sigma({
         graph: g,
         renderer: { container: document.getElementById('ViewContainer'), type: 'canvas' },
-        settings: { labelColor: 'node', labelSize: 'fixed', labelThreshold: 5 }
+        settings: { labelColor: 'node', labelSize: 'fixed', labelThreshold: 4 }
     });
 
     s.refresh();
