2227d678a1a08c85fafb11ee48b46afa5b467b77
Jaron <jaronpiet@t-online.de>
2015-02-09 20:05:47 +0100

    Extract the layouting
    The layout "strategy" is extracted from the SigmaJS-View to make it more
    flexible. The current layout algorithm depends on the number of
    references of a node.

--- SigmaView.js
+++ SigmaView.js
@@ -1,89 +1,20 @@
-function NewNode(nodeList)
+
+//ViewInterface candidate
+function UpdateView(nodeList)
 {
-    var i,
-    s,
-    g = {
-        nodes: [],
-        edges: []
-    };
-   
-    var LabelOffsetFactor = 5.5;
-
-    nodeList.sort(SortByRank);
-    var maxReferences = nodeList[0].NumReferences+1;
-    var graphLevelMatrix = new Array(maxReferences);
-
-    for (i = 0; i < nodeList.length; i++)
-    {
-        var node = nodeList[i];
-        if (graphLevelMatrix[node.NumReferences] == null)
-            graphLevelMatrix[node.NumReferences] = new Array();
-
-        graphLevelMatrix[node.NumReferences].push(node);
-    }
-
-    for (var level = 0; level < maxReferences; level++)
-    {
-        var numLevelNodes = 0;
-        if (graphLevelMatrix[level] != null)
-            numLevelNodes = graphLevelMatrix[level].length;
-
-        var widthVC = document.getElementById("ViewContainer").clientWidth;
-        var nodeStep = widthVC / (numLevelNodes);
-
-        var xPos = -(nodeStep * (numLevelNodes+1)/ 2);
-        var yPos = 0;
-        for (i = 0; i < numLevelNodes; i++)
-        {
-            var node = graphLevelMatrix[level][i];
-            var nodeName = node.Name;
-
-            xPos += nodeStep;
-            yPos = - level * 100;
-
-            g.nodes.push({
-                id: nodeName,
-                label: nodeName,
-                x: xPos,
-                y: yPos,
-                size: level + 5,
-                'color': 'rgb(' + Math.round(level * 30) + ',' +
-                                  Math.round(256) + ',' +
-                                  Math.round(level * 30) + ')'
-            });
-
-            for (j = 0; j < node.NumReferences; j++)
-                g.edges.push({
-                    id: 'e' + nodeName + j,
-                    source: node.References[j],
-                    target: nodeName,
-                    size: 0.1,
-                    'color': 'rgb(' + Math.round(level * 20) + ',' +
-                                      Math.round(level * 50) + ',' +
-                                      Math.round(256) + ')'
-                });
-        }
-    }
+    var g = CreateGraph(nodeList);
+    RefreshGraph(g);
+}
 
 
-
-    // Instantiate sigma:
-    s = new sigma({
-        graph: g,
+function RefreshGraph(graph)
+{
+    document.getElementById('ViewContainer').innerHTML = "";
+    var s = new sigma({
+        graph: graph,
         renderer: { container: document.getElementById('ViewContainer'), type: 'canvas' },
         settings: { labelColor: 'node', labelSize: 'fixed', labelThreshold: 4 }
     });
 
     s.refresh();
 }
-
-
-function SortByRank(a, b)
-{
-    if (a.NumReferences <= b.NumReferences)
-        return 1;
-    else
-        return -1;
-}
-
-
---
+++ layout/SigmaRefTreeLayout.js
@@ -0,0 +1,99 @@
+
+//LayoutInterface candidate
+function CreateGraph(nodeList)
+{
+    var g =
+    {
+        nodes: [],
+        edges: []
+    };
+
+    nodeList.sort(SortByInfluence);
+
+    var maxReferences = nodeList[0].NumReferences + 1;
+    var levelColorStep = 256 / maxReferences;
+
+
+    var graphLevelMatrix = CreateGraphLevelMatrix(nodeList, maxReferences);
+
+    for (var level = 0; level < maxReferences; level++)
+    {
+        var numLevelNodes = CalculateNumLevelNodes(graphLevelMatrix, level);
+        var nodeStep = CalculateNodeXStep(numLevelNodes);
+
+        var xPos = -(nodeStep * (numLevelNodes + 1) / 2);
+        var yPos = 0;
+        for (i = 0; i < numLevelNodes; i++)
+        {
+            var node = graphLevelMatrix[level][i];
+            var nodeName = node.Name;
+
+            xPos += nodeStep;
+            yPos = -level * 100;
+
+            g.nodes.push({
+                id: nodeName,
+                label: nodeName,
+                x: xPos,
+                y: yPos,
+                size: level + 5,
+                'color': 'rgb(' + Math.round((level + 1) * levelColorStep) + ',' +
+                                  Math.round(256) + ',' +
+                                  Math.round((level + 1) * levelColorStep) + ')'
+            });
+
+            for (j = 0; j < node.NumReferences; j++)
+                g.edges.push({
+                    id: 'e' + nodeName + j,
+                    source: node.References[j],
+                    target: nodeName,
+                    size: 0.1,
+                    'color': 'rgb(' + Math.round((level + 1) * levelColorStep) + ',' +
+                                      Math.round((level + 1) * levelColorStep) + ',' +
+                                      Math.round(256) + ')'
+                });
+        }
+    }
+
+    return g;
+}
+
+
+function CreateGraphLevelMatrix(nodeList, maxReferences)
+{
+    var graphLevelMatrix = new Array(maxReferences);
+    for (i = 0; i < nodeList.length; i++)
+    {
+        var node = nodeList[i];
+        if (graphLevelMatrix[node.NumReferences] == null)
+            graphLevelMatrix[node.NumReferences] = new Array();
+
+        graphLevelMatrix[node.NumReferences].push(node);
+    }
+
+    return graphLevelMatrix;
+}
+
+function CalculateNumLevelNodes(graphLevelMatrix, level)
+{
+    var numLevelNodes = 0;
+    if (graphLevelMatrix[level] != null)
+        numLevelNodes = graphLevelMatrix[level].length;
+
+    return numLevelNodes;
+}
+
+function CalculateNodeXStep(numLevelNodes)
+{
+    var widthVC = document.getElementById("ViewContainer").clientWidth;
+    return widthVC / (numLevelNodes);
+}
+
+
+function SortByInfluence(a, b)
+{
+    if (a.NumReferences <= b.NumReferences)
+        return 1;
+    else
+        return -1;
+}
